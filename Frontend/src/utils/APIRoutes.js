b24c2a5c20d99e5d2c91e607ab1e6ed533b40f35
dilipdevops5511 <dilipdevopscourses@gmail.com>
2024-05-20 22:12:17 +0530

    ;;;

--- Frontend/src/utils/APIRoutes.js
+++ Frontend/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = process.env.REACT_APP_BACKEND_URL;docker build -t frontend .
+export const host = process.env.REACT_APP_BACKEND_URL;
 
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
