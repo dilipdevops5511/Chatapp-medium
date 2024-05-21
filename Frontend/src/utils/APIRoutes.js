70cd11535a0d0e5f74e44addee15e74245001177
dilipdevops5511 <dilipdevopscourses@gmail.com>
2024-05-21 11:30:02 +0530

    j

--- Frontend/src/utils/APIRoutes.js
+++ Frontend/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = process.env.REACT_APP_BACKEND_URL;
+export const host = "http://10.100.247.167:5000";
 
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
