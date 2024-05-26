9a45741cd8eb08ce515789577e333daa58f9863b
dilipdevops5511 <dilipdevopscourses@gmail.com>
2024-05-26 21:23:29 +0530

    check out

--- Frontend/src/utils/APIRoutes.js
+++ Frontend/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://10.100.71.151:5000";
+export const host = "http://a414bbb94e84e419eaae85945853962a-373392892.us-east-1.elb.amazonaws.com:5000";
 
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
