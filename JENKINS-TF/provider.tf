a516d38a1be65377a782fe6869894908b0272f70
dilipdevops5511 <dilipdevopscourses@gmail.com>
2024-06-06 21:05:59 +0530

    added terraform provider

--- JENKINS-TF/provider.tf
+++ JENKINS-TF/provider.tf
@@ -7,7 +7,11 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
