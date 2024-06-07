338bfc278a7a2897f73bbf850a836630b6cbc62b
dilipdevops5511 <dilipdevopscourses@gmail.com>
2024-06-07 23:03:49 +0530

    ingrass

--- Backend/index.js
+++ Backend/index.js
@@ -15,12 +15,10 @@ const PORT = process.env.PORT || 5000;
 // Use environment variables for MongoDB credentials
 const MONGO_USERNAME = process.env.MONGO_USERNAME;
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
-console.log("MongoDB Username:", MONGO_USERNAME);
-console.log("MongoDB Password:", MONGO_PASSWORD);
 
 // Construct MongoDB connection string using the credentials
 const MONGO_CONN_STR = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.n7msjwt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log("MongoDB Connection String:", MONGO_CONN_STR);
+
 
 const startServer = () => {
   app.use(cors());
