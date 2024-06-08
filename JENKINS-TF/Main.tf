05f19c9ca53e80f60904370a6858dc555693fd8f
dilipdevops5511 <dilipdevopscourses@gmail.com>
2024-06-08 12:11:47 +0530

    k

--- JENKINS-TF/Main.tf
+++ JENKINS-TF/Main.tf
@@ -29,7 +29,7 @@ resource "aws_security_group" "Jenkins-sg" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0c7217cdde317cfec"
+  ami                    = "ami-036cafe742923b3d9"
   instance_type          = "t2.large"
   key_name               = "my key"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
@@ -45,7 +45,7 @@ resource "aws_instance" "web" {
 }
 
 resource "aws_instance" "web2" {
-  ami                    = "ami-0c7217cdde317cfec"
+  ami                    = "ami-036cafe742923b3d9"
   instance_type          = "t2.medium"
   key_name               = "my key"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
