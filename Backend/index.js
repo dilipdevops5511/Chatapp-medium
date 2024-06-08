bf0ae31648cfe64dac280b503a8f8827a677f3b3
dilipdevops5511 <dilipdevopscourses@gmail.com>
2024-06-08 08:24:39 +0530

    added app name as env

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
 
 // Construct MongoDB connection string using the credentials
 const MONGO_CONN_STR = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.n7msjwt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
+console.log(MONGO_CONN_STR)
 
 const startServer = () => {
   app.use(cors());
