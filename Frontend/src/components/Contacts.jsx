3b561c6f0d3a385884ba7bb619654ec6b5a3ed33
dilipdevops5511 <dilipdevopscourses@gmail.com>
2024-06-09 20:26:48 +0530

    frontend

--- Frontend/src/components/Contacts.jsx
+++ Frontend/src/components/Contacts.jsx
@@ -23,7 +23,7 @@ export default function Contacts({ contacts, changeChat }) {
         <Container>
           <div className="brand">
             <img src={Logo} alt="logo" />
-            <h3>Chat Genius</h3>
+            <h3>Chat Genius APP</h3>
           </div>
           <div className="contacts">
             {contacts.map((contact, index) => {
